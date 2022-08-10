45bb9eefc8c0e9c5c8c01722b697400ac0e018ca
Senor-Ducky <robom460@gmail.com>
2022-08-10 15:47:51 +0530

    complete

---
+++ src/components/Circle/circle.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './circle.css';
+
+export default ({color}) => <div class="circle" style={color}></div>;
---
+++ src/components/Light/light.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import './light.css';
+import Circle from '../Circle/circle';
+
+const colors = {
+  red: {
+    backgroundColor: "#cc3232"
+  },
+  yellow: {
+    backgroundColor: "#e7b416"
+  },
+  green: {
+    backgroundColor: "#2dc937"
+  },
+  grey: {
+    backgroundColor: "grey"
+  }
+};
+
+var redDone = false;
+var greenDone = false;
+var yellowDone = false;
+
+class Light extends React.Component {
+  state = {
+      red: colors.red,
+      yellow: colors.grey,
+      green: colors.grey,
+      next: 'yellow'
+    }
+
+    
+
+
+    handeLightChange = () => {
+      switch (this.state.next) {
+        case "red":
+          this.setState({
+            red: colors.grey,
+            yellow: colors.grey,
+            green: colors.grey,
+            next: 'green'
+          });
+          break;
+        case "yellow":
+          this.setState({
+            red: colors.grey,
+            yellow: colors.grey,
+            green: colors.green,
+            next: 'red'
+          });
+          break;
+        case "green":
+          this.setState({
+            red: colors.red,
+            yellow: colors.grey,
+            green: colors.grey,
+            next: 'yellow'
+          });
+          break;
+      }
+    }
+
+    greenClick = () => {
+      this.setState({
+        red: colors.grey,
+        yellow: colors.grey,
+        green: colors.green,
+        
+      });
+      redDone = true;
+    }
+
+    yellowClick = () => {
+      this.setState({
+        red: colors.grey,
+        yellow: colors.yellow,
+        green: colors.grey,
+        
+      });
+
+      greenDone = true;
+    }
+    
+    redClick = () => {
+      this.setState({
+        red: colors.red,
+        yellow: colors.grey,
+        green: colors.grey,
+        
+      });
+
+      greenDone = true;
+      yellowDone = true
+    }
+
+   
+
+    componentDidMount() {
+
+      if( redDone === false) {
+        setInterval(()=>{
+          this.handeLightChange()
+          redDone = true;
+        }, 20000)
+      } 
+
+      if( redDone === true && greenDone === false) {
+        setInterval(()=>{
+          this.handeLightChange()
+          greenDone = true;
+        }, 15000)
+      }
+
+      if( redDone === true && greenDone === true && yellowDone === false) {
+        setInterval(()=>{
+          this.handeLightChange()
+          yellowDone = true;
+        }, 5000)
+      }
+
+      redDone = false;
+      greenDone = false;
+      yellowDone = false;
+      
+      }
+    
+
+    
+    
+
+
+  render() {
+    return (
+      <div>
+      <div class="light">
+        <Circle color={this.state.red}/>
+        <Circle color={this.state.yellow}/>
+        <Circle color={this.state.green}/>
+      </div>
+      <button onClick={this.greenClick} className='greenBtn'>green</button>
+      <button onClick={this.yellowClick} className='yellowBtn'>yellow</button>
+      <button onClick={this.redClick} className='redBtn'>red</button>
+      </div>
+
+      
+    )
+  }
+}
+
+export default Light;
